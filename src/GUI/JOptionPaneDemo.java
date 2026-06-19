b23d975f9ec61a9239c72f2d7637b095cb6e3ac5
Eshal <eshalnaeem2008@gmail.com>
2026-06-19 19:10:19 +0500

    add JOptionPane Dialog example

---
+++ src/GUI/JOptionPaneDemo.java
@@ -0,0 +1,20 @@
+package GUI;
+
+import javax.swing.*;
+
+public class JOptionPaneDemo {
+    public static void main(String args[]){
+
+        //JOptionPane.showMessageDialog(null,"This is some info","title",JOptionPane.PLAIN_MESSAGE);
+       // JOptionPane.showMessageDialog(null,"This is some info","title",JOptionPane.INFORMATION_MESSAGE);
+       //JOptionPane .showMessageDialog(null,"What?","title",JOptionPane.QUESTION_MESSAGE);
+        //JOptionPane.showMessageDialog(null,"Your Computer has a VIRUS !!!","TITLE",JOptionPane.WARNING_MESSAGE);
+       // JOptionPane.showMessageDialog(null,"Call a tech support now or else","title",JOptionPane.ERROR_MESSAGE);
+
+        //int answer=JOptionPane.showConfirmDialog(null,"bro do you even code ? ","This is my title",JOptionPane.YES_NO_CANCEL_OPTION);
+
+       // String name=JOptionPane.showInputDialog("What is your name ?");
+       //JOptionPane.showOptionDialog(null,"You are awesome !"," secret message",JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.INFORMATION_MESSAGE,null,null,0);
+
+    }
+}
