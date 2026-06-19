f85032b59f939b770674671702bb803f26fd6a66
Eshal <eshalnaeem2008@gmail.com>
2026-06-19 19:10:48 +0500

    implement keyListener Example

---
+++ src/GUI/KeyListenerDemo.java
@@ -0,0 +1,77 @@
+package GUI;
+
+
+import java.awt.Color;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+class MyFrames extends JFrame implements java.awt.event.KeyListener {
+    JLabel label;
+
+    MyFrames(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(null);
+        this.addKeyListener(this);
+
+        label=new JLabel();
+        label.setBounds(0,0,100,100);
+        label.setBackground(Color.red);
+        label.setOpaque(true);
+
+        this.add(label);
+        this.setFocusable(true);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        switch(e.getKeyChar()){
+            case 'a': label.setLocation(label.getX()-1, label.getY());
+                break;
+
+            case 'w': label.setLocation(label.getX(), label.getY()-1);
+                break;
+
+            case 's': label.setLocation(label.getX(), label.getY()+1);
+                break;
+
+            case 'd': label.setLocation(label.getX()+1, label.getY()-1);
+                break;
+        }
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        char c=e.getKeyChar();
+
+        switch(c){
+            case 'a': label.setLocation(label.getX()-10,label.getY());
+                break;
+
+            case 'w': label.setLocation(label.getX(),label.getY()-10);
+                break;
+
+            case 's': label.setLocation(label.getX(), label.getY()+10);
+                break;
+
+            case 'd': label.setLocation(label.getX()+10, label.getY());
+                break;
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        System.out.println("You released key char : " + e.getKeyChar());
+
+    }
+}
+public class KeyListenerDemo {
+    public static void main(String args[]) {
+        new MyFrames();
+
+    }
+}
